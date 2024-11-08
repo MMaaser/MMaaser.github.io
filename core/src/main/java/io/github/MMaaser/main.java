5353a2153856d142577f9eee13345fd38ed892a8
Maxwell <111320153+MMaaser@users.noreply.github.com>
2024-11-08 13:12:58 -0500

    Update main.java
    yadda yadda

--- core/src/main/java/io/github/MMaaser/main.java
+++ core/src/main/java/io/github/MMaaser/main.java
@@ -1,34 +1,73 @@
-package io.github.MMaaser;
-
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class main implements ApplicationListener {
+    //assets
+    Texture backgroundTexture;
+    Texture dropTexture;
+    Texture bucketTexture;
+    Sound dropSound;
+    Music music;
 
+    SpriteBatch spriteBatch; //combines all drawing calls (separate textures) into one to not kill the gpu
+    FitViewport viewPort; //window size and zoom
+    //define vars
     @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+    public void create(){
+        backgroundTexture = new Texture("background.png");
+        dropTexture = new Texture("drop.png");
+        bucketTexture = new Texture("bucket.png");
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+
+        spriteBatch = new SpriteBatch();
+        viewPort = new FitViewport(8,5);
+    }
+
+    public void render(){
+        input();
+        logic();
+        draw();
+    }
+
+    private void input(){
+
+    }
+    private void logic(){
+
+    }
+    //draws every frame
+    private void draw(){
+        ScreenUtils.clear(Color.BLACK);
+        viewPort.apply();
+        spriteBatch.setProjectionMatrix(viewPort.getCamera().combined); //put images in teh right place
+
+        float worldWidth = viewPort.getWorldWidth();
+        float worldHeight= viewPort.getWorldHeight();
+
+        spriteBatch.begin();
+
+        //draw things here
+        spriteBatch.draw(backgroundTexture, worldWidth, worldHeight);
+        spriteBatch.draw(bucketTexture, 0,0,1,1);
+
+        spriteBatch.end();
     }
 
     @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+    public void resize (int height, int width){}
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+    public void pause(){}
+
+    public void resume(){}
+
+    public void dispose(){}
 }
