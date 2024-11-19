c6b1084fda417c8de3d5d00cfcbafb61bdf405e1
Maxwell <maasmax1@reedcityschools.org>
2024-11-19 08:22:48 -0500

    Update Level.js

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -1,5 +1,7 @@
 
 // You can write more code here
+var walker;
+var keypad;
 
 /* START OF COMPILED CODE */
 
@@ -13,31 +15,41 @@ class Level extends Phaser.Scene {
 		/* END-USER-CTR-CODE */
 	}
 
+	preload(){
+		this.load.image("guapen", "assets/guapen");
+
+	}
+
 	/** @returns {void} */
 	editorCreate() {
-
-        //bg
-       /* const bg = this.add.image(0,0,"background1");
-
-	    //player
-	    const walker = this.physics.add.sprite(100,100,"mainSprite");
-	    walker.setInteractive();*/
-
+	
 		// dino
 		const dino = this.add.image(640, 288, "dino");
 		dino.setInteractive(new Phaser.Geom.Rectangle(0, 0, 250, 250), Phaser.Geom.Rectangle.Contains);
 
+		const walker = this.add.image(300,300, "guapen");
+		walker.setInteractive();
+
+		keypad = this.input.keyboard.createCursorKeys();
+
+		//walker.setCollideWorldBounds(true);
+
 		// onPointerDownScript
 		const onPointerDownScript = new OnPointerDownScript(dino);
 
+		//const onWalkerDownScript = new On
+
 		// pushActionScript
 		new PushActionScript(onPointerDownScript);
+		///new PushActionScript(onWalkerDownScript);
 
 		// onAwakeScript
 		const onAwakeScript = new OnAwakeScript(dino);
+		//const onWakingWalkerScript = new onAwakeScript(walker);
 
 		// moveInSceneActionScript
 		const moveInSceneActionScript = new MoveInSceneActionScript(onAwakeScript);
+		//const moveWalkerActionScript = new MoveInSceneActionScript(onWakingWalkerScript);
 
 		// welcome
 		const welcome = this.add.text(640, 478, "", {});
@@ -53,6 +65,7 @@ class Level extends Phaser.Scene {
 
 		// moveInSceneActionScript (prefab fields)
 		moveInSceneActionScript.from = "TOP";
+		//moveWalkerActionScript.from = "TOP";
 
 		// moveInSceneActionScript (components)
 		const moveInSceneActionScriptDurationConfigComp = new DurationConfigComp(moveInSceneActionScript);
@@ -66,42 +79,32 @@ class Level extends Phaser.Scene {
 		fadeActionScriptDurationConfigComp.duration = 1500;
 
 		this.events.emit("scene-awake");
-
-		cursors.this.input.keyboard.createCursorKeys();
+		
 	}
 
 	/* START-USER-CODE */
+	
 
 	// Write more your code here
-
-	/*update(){
-	if(cursors.left.isDown){
-	walker.setVelocityX(-100);
-	walker.setVelocityY(0);
-	} else if (cursors.right.isDown){
-	walker.setVelocityX(100);
-	walker.setVelocityY(0);
-	} else if (cursors.up.isDown){
-	walker.setVelocityY(100);
-	walker.setVelocityX(0);
-	} else if (cursors.down.isDown){
-	walker.setVelocityY(-100);
-	walker.setVelocityY(0);
-	}
-	else {
-	walker.setVelocityX(0);
-	walker.setVelocityY(0);
-	}
-	}*/
 
 	create() {
 
 		this.editorCreate();
 	}
 
+	update(){
+
+		var isUpDown = keypad.up.isDown;
+		var isDownDown = keypad.down.isDown;
+		var isLeftDown = keypad.left.isDown;
+		var isRightDown = keypad.right.isDown;
+		var spaceDown = keypad.space.isDown;
+
+		if (keypad.space.isDown){
+			walker.setVelocityX(200);
+		}
+	}
+
 	/* END-USER-CODE */
+	
 }
-
-/* END OF COMPILED CODE */
-
-// You can write more code here
